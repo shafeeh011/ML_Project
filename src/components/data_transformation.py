6796389578fe8a6e1c2dc247aa38bb55405ca805
shafeeh011 <shafeeh011@.com>
2025-01-12 17:49:11 +0530

    data transformation addd

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -13,6 +13,8 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from src.exception import CustomException
 from src.logger import logging
 
+from src.utils import save_object
+
 @dataclass
 class DataTransformationConfig:
     preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
@@ -20,6 +22,7 @@ class DataTransformationConfig:
 
 class DataTransformation:
     def __init__(self): 
+        
         self.data_transformation_config=DataTransformationConfig()
         
     def get_data_transformation_object(self):
@@ -87,9 +90,22 @@ class DataTransformation:
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
             
-        
-        
-        
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+            
+            logging.info(f"Saved preprocessing object")
+            
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+            
+            return(
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path
+            )
+            
         
         except Exception as e:
             raise CustomException(e,sys)    
